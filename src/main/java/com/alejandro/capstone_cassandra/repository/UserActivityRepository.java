3bead63ea45c40edd8364f0f3d89c9a392e1e7e5
AlejandroCoronadoRubio <alejandrocoronadorubio@gmail.com>
2025-07-11 07:20:21 -0700

    Modifications on consistency and contact points

--- src/main/java/com/alejandro/capstone_cassandra/repository/UserActivityRepository.java
+++ src/main/java/com/alejandro/capstone_cassandra/repository/UserActivityRepository.java
@@ -1,9 +1,7 @@
 package com.alejandro.capstone_cassandra.repository;
 
 import com.alejandro.capstone_cassandra.entity.UserActivity;
-import com.datastax.oss.driver.api.core.DefaultConsistencyLevel;
 import org.springframework.data.cassandra.repository.CassandraRepository;
-import org.springframework.data.cassandra.repository.Consistency;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
@@ -12,13 +10,8 @@ import java.util.UUID;
 
 @Repository
 public interface UserActivityRepository extends CassandraRepository<UserActivity, UUID> {
-    //@Query(value = "SELECT * FROM user_activity WHERE userId = :userId AND timestamp = :timestamp; ")
-    @Consistency(DefaultConsistencyLevel.LOCAL_ONE)
     List<UserActivity> findAllByUserId(UUID userId);
-    @Consistency(DefaultConsistencyLevel.LOCAL_ONE)
     List<UserActivity> findAllByUserIdAndTimestamp(UUID userId, LocalDateTime timestamp);
-    @Consistency(DefaultConsistencyLevel.LOCAL_ONE)
     List<UserActivity> findAllByUserIdAndTimestampAfter(UUID userId, LocalDateTime timestamp);
-    @Consistency(DefaultConsistencyLevel.ALL)
     void deleteByUserIdAndTimestampBefore(UUID userId, LocalDateTime timestamp);
 }
